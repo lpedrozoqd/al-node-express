dae4ddb4c1a8e58e5e8c99477625c3801cf175bc
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-08-16 23:29:25 -0500

    Uso de get-query y fakers (data random)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const faker = require('faker');
 
 //**Clase #1
 //Aquí para iniciar, aplicarmos una ruta:
@@ -15,12 +16,33 @@ app.get('/nueva-ruta',(req,res)=>{
 });
 
 app.get('/products',(req,res)=>{
+  //uso de faker
+  const products = [];
+  const {size} = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price()),
+      image: faker.image.imageUrl()
+    });
+  }
+  //Respuesta con base en faker (generación de datos randómico)
+  res.json(products);
+
   //Aquí enviaremos otro tipo de formato. JSON en este caso:
-  res.json({
-     name:"Product 1" ,
-     price:400
-  });
+  // res.json({
+  //    name:"Product 1" ,
+  //    price:400
+  // });
 });
+
+//Se deben colocar primero las rutas estáticas y debajo las rutas dinámicas (clase #3)
+app.get('/products/filter',(req,res)=>{
+  res.send("soy un filter!");
+});
+
 
 //**Clase #3
 app.get('/products/:id',(req,res)=>{
@@ -47,6 +69,20 @@ app.get('/categories/:categoryId/products/:productId',(req,res)=>{
     name:"product x" ,
     price:400
   });
+});
+
+//**Clase #4
+app.get('/users',(req,res)=>{
+  //Ejemplo de estrategia de paginación
+  const {limit, offset} = req.query;
+  if (limit && offset){
+    res.json({
+      limit,
+      offset
+    });
+  }else{
+    res.send('Nohay parámetros!');
+  }
 });
 
 
