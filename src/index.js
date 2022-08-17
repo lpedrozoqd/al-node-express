da6ea164e6a04e1b0609fe8a444e86d96ce94d89
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-08-16 21:32:11 -0500

    Más ejemplos de rutas y tipo de respuesta (uso de json)

--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//**Clase #1
 //Aquí para iniciar, aplicarmos una ruta:
 app.get('/',(req,res)=>{
   res.send("Hola!, mi server Express");
 });
 
-//eslint resalta 'console.log' por aquello de que no es una buena práctica dejarlo 
+
+//**Clase #2
+app.get('/nueva-ruta',(req,res)=>{
+  res.send("Hola!, soy una nueva ruta!");
+});
+
+app.get('/productos',(req,res)=>{
+  //Aquí enviaremos otro tipo de formato. JSON en este caso:
+  res.json({
+     name:"Product 1" ,
+     price:400
+  });
+});
+
+
+
+//eslint resalta 'console.log' por aquello de que no es una buena práctica dejarlo
 //para ambiente productivo
 app.listen(port, ()=>{
-  console.log("Mi port: " + port);
+  console.log(`Mi port:  http://localhost:${port}`);
 });
+
 
