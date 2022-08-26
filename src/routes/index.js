15342b53592d16dfb8b693aa95c6eec979859656
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-08-26 18:37:36 -0500

    Manejo de versionamiento de las rutas /api/v1...v2...vn

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,5 @@
+const express = require('express');
+
 const routerProducts = require('./productsRouter');
 const routerUser = require('./usersRouter');
 const routerCategories = require('./categoriesRouter');
@@ -6,11 +8,22 @@ const rootRuta = require('./rootRouter');
 
 
 function routerAPI(app) {
+  //De esta manera se usa sin tener en cuenta el versiomaiento. Ej: /api/v1...v2...vn
+  /*
   app.use('/products',routerProducts);
   app.use('/users',routerUser);
   app.use('/categories',routerCategories);
   app.use('/nueva-ruta',routerNuevaRuta);
+  //Con esta opción se muestra la página inicial con sólo http://localhost:3000
   app.use('/',rootRuta);
+  */
+  const router = express.Router();
+  app.use('/api/v1',router);
+  router.use('/products',routerProducts);
+  router.use('/users',routerUser);
+  router.use('/categories',routerCategories);
+  router.use('/nueva-ruta',routerNuevaRuta);
+  router.use('/',rootRuta);
 }
 
 module.exports = routerAPI;
