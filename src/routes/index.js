a817818c11fb7fac98f49723f6311d701c02a942
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-08-26 18:26:51 -0500

    Reorganización de Rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 const routerProducts = require('./productsRouter');
 const routerUser = require('./usersRouter');
+const routerCategories = require('./categoriesRouter');
+const routerNuevaRuta = require('./nuevaRutaRouter');
+const rootRuta = require('./rootRouter');
 
 
 function routerAPI(app) {
   app.use('/products',routerProducts);
   app.use('/users',routerUser);
+  app.use('/categories',routerCategories);
+  app.use('/nueva-ruta',routerNuevaRuta);
+  app.use('/',rootRuta);
 }
 
 module.exports = routerAPI;
