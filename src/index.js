ab1f7381adbd075094ec5998c98581e766a18f27
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-08-27 21:21:06 -0500

    Uso de 'cors'

--- src/index.js
+++ src/index.js
@@ -3,10 +3,29 @@ const app = express();
 const port = 3000;
 const routerAPI = require('./routes');
 const {logErrors,errorHandler, boomErrorHandler} = require('./middleware/errorHandler');
+const cors = require('cors');
 
 //Esto es el uso de 'middleware', es decir, se está declarando que por }
 //ejemplo para los Post se reciban los msjs en formato json
 app.use(express.json());
+
+const whitList = [
+  'http://localhost:8080',
+  'https://sb.co'
+];
+const options = {
+  origin:(origin,callback) =>{
+    if (whitList.includes(origin)){
+      callback(null,true);
+    }else{
+      callback(new Error('No permitido!'));
+    }
+  }
+}
+
+//De esta manera, acepta cualquier
+app.use(cors(options));
+
 
 app.get('/',(req,res)=>{
   res.send('<h1>Hola Express</h1>');
